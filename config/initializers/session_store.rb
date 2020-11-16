e5688ff5156797bb8520fbeca75218a594e6a379
Pablo <pledesma@wordstream.com>
2020-11-15 22:04:07 -0500

    env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
-# the key is the name of the session cookie
-# TODO prod configuration
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "something_something-app-api.herokuapp.com"
+if Rails.env == "production"
+  # the key is the name of the session cookie
+  # TODO prod configuration
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "something_something-app-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
