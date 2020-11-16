fd69dac4394ea98dc5b1f2470692a19f14d8049e
Pablo <pledesma@wordstream.com>
2020-11-15 21:38:03 -0500

    session return

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,17 @@ class SessionsController < ApplicationController
     user = User
       .find_by(email: params["user"]["email"])
       .try(:authenticate, params["user"]["password"])
+
+    if user
+      # communicates with the session and store the user id encrypted in the client app
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
   end
 end
