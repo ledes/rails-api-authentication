3b24aa1ef8c86fee1f1d3c3cbd929e5cedb35f72
Pablo <pledesma@wordstream.com>
2020-11-15 20:43:23 -0500

    set up with key elements for auth

---
+++ config/initializers/cors.rb
@@ -0,0 +1,15 @@
+# Gives the ability to whitelist domains.
+# With rails api working with session and credentials needs to setup cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost::3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+
+  # TODO prod configuration
+  allow do
+    origins "https://something_something.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# the key is the name of the session cookie
+# TODO prod configuration
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "something_something-app-api.herokuapp.com"
